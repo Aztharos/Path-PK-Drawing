228bf89d3f68044be5e5033e322d05065d3df39b
Aztharos <166838533+Aztharos@users.noreply.github.com>
2024-12-12 20:48:17 +0100

    Create Acceuil.swift

---
+++ Acceuil.swift
@@ -0,0 +1,93 @@
+import SwiftUI
+import UIKit
+
+struct Accueil: View {
+    var body: some View {
+        NavigationView {
+            ZStack {
+                VStack {
+                VStack {
+                  
+                    NavigationLink(destination: DrawingView()) {
+                        Text("WhiteBoard")
+                            .font(.custom("AnimeAce2.0BB-Bold", size: 20))
+                            .foregroundColor(.purple)
+                            .fontWeight(.bold)
+                            .padding(10)
+                            .background(
+                                BlurBackground()
+                            )
+                    }
+                    .padding(10)
+                }
+                Spacer()
+            }
+                }
+            Image("background")
+                .resizable()
+                .scaledToFill()
+            }
+        }
+    }
+
+struct BlurView: UIViewRepresentable {
+    func makeUIView(context: Context) -> UIVisualEffectView {
+        let view = UIVisualEffectView(effect: UIBlurEffect(style: .systemUltraThinMaterial))
+        view.layer.cornerRadius = 20
+        view.layer.masksToBounds = true
+        return view
+    }
+    
+    func updateUIView(_ uiView: UIVisualEffectView, context: Context) {
+        uiView.effect = UIBlurEffect(style: .systemUltraThinMaterial)
+    }
+}
+
+struct BlurBackground: View {
+  
+    var body: some View {
+        BlurView()
+            .overlay(
+                RoundedRectangle(cornerRadius: 20)
+                    .stroke(Color.white.opacity(0.2), lineWidth: 1)
+            )
+            .shadow(color: Color.black.opacity(0.1), radius: 10, x: 0, y: 10)
+            .cornerRadius(10)
+            .shadow(color: .gray, radius: 5, x: 0, y: 2)
+    }
+}
+
+struct StyledButton: View {
+  
+    let systemImage: String
+    let action: () -> Void
+    @State private var isPressed = false
+    var body: some View {
+        Button(action: action) {
+            Image(systemName: systemImage)
+                .font(.title3)
+                .foregroundStyle(
+                    LinearGradient(
+                        gradient: Gradient(colors: [.white, .yellow]),
+                        startPoint: .leading,
+                        endPoint: .top
+                    )
+                )
+             
+                .padding(10)
+                .background(
+                    BlurBackground()
+                )
+                .overlay(
+                    RoundedRectangle(cornerRadius: 16)
+                        .stroke(isPressed ? Color.white : Color.clear, lineWidth: 8)
+                        .animation(.easeInOut(duration: 0.4), value: isPressed)
+                )
+        }
+        .onLongPressGesture(minimumDuration: 0.8, pressing: { isPressing in
+            withAnimation {
+                isPressed = isPressing
+            }
+        }, perform: {})
+    }
+}
